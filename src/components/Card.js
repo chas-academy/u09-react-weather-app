75b4659fd37c2c2211e4cc17f7edbce2cb57a880
jpablo1980 <esperix@gmail.com>
2020-04-09 13:53:43 +0200

    Geolocation works in the main card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,66 +1,77 @@
 import React from 'react';
 import Axios from 'axios';
 import UserLocation from './UserLocation';
-import { usePosition } from 'use-position';
 var moment = require('moment');
 
 export default class Card extends React.Component {
 
   state = {
     coords: {
-      latitude: 35,
-      longitude: 139,
+      latitude: null,
+      longitude: null,
     },
     data: {}
   }
 
   componentDidMount() {
 
-    navigator.geolocation.getCurrentPosition((position) => {
-      let newCoords = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      }
-      this.setState({ coords: newCoords });
-      console.log(newCoords.latitude);
+    //cache this so it can be used inside a callback function
+    let currenComponent = this;
 
-      // Get device geolocation
-      if (newCoords.latitude !== null) {
-        Axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=stockholm&units=metric&appid=5a274b56354a707ffc91ac0c8eec0c72&query=
-        ${this.state.coords.latitude}, ${this.state.coords.longitude}`).then(res => {
-          console.log(res);
-          let weatherData = {
-            location: res.data.city.name,
-            country: res.data.city.country,
-            temperature: Math.floor(res.data.list[0].main.temp),
-            description: res.data.list[0].weather[0].main,
-            wind_speed: res.data.list[0].wind.speed,
-            humidity: res.data.list[0].main.humidity,
-            sunrise: moment.unix(res.data.city.sunrise).format("HH:MM"),
-            sunset: moment.unix(res.data.city.sunset).format("HH:MM"),
+    //gets permisions from geolocation
+    navigator.permissions.query({
+      name: 'geolocation'
+    }).then(function (result) {
+
+      if (result.state == 'granted') {
+        console.log("Granted");
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          let newCoords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
           }
-          this.setState({ data: weatherData });
+          currenComponent.setState({ coords: newCoords });
+          console.log("Geolocation Supported");
+
+          Axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${currenComponent.state.coords.latitude}&lon=${currenComponent.state.coords.longitude}&units=metric&appid=5a274b56354a707ffc91ac0c8eec0c72`)
+            .then(res => {
+              let weatherData = {
+                location: res.data.city.name,
+                country: res.data.city.country,
+                temperature: Math.floor(res.data.list[0].main.temp),
+                description: res.data.list[0].weather[0].main,
+                wind_speed: res.data.list[0].wind.speed,
+                humidity: res.data.list[0].main.humidity,
+                sunrise: moment.unix(res.data.city.sunrise).format("HH:MM"),
+                sunset: moment.unix(res.data.city.sunset).format("HH:MM"),
+              }
+              currenComponent.setState({ data: weatherData });
+            });
         })
-      
-      } else {
-        console.log('Geolocation Not Supported');
-        Axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=stockholm&units=metric&appid=5a274b56354a707ffc91ac0c8eec0c72&query=
-      ${this.state.coords.latitude}, ${this.state.coords.longitude}`).then(res => {
-          console.log(res);
-          let weatherData = {
-            location: res.data.city.name,
-            country: res.data.city.country,
-            temperature: res.data.list[0].main.temp,
-            description: res.data.list[0].weather[0].main,
-            wind_speed: res.data.list[0].wind.speed,
-            humidity: res.data.list[0].main.humidity,
-            sunrise: res.data.city.sunrise,
-            sunset: res.data.city.sunset,
-          }
-          this.setState({ data: weatherData });
-        })
+
+      } else if (result.state !== 'granted') {
+        console.log("No Granted");
+
+        console.log('Geolocation Not Supported')
+        Axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=berlin&units=metric&appid=5a274b56354a707ffc91ac0c8eec0c72`)
+          .then(res => {
+            console.log(res);
+            let weatherData = {
+              location: res.data.city.name,
+              country: res.data.city.country,
+              temperature: Math.floor(res.data.list[0].main.temp),
+              description: res.data.list[0].weather[0].main,
+              wind_speed: res.data.list[0].wind.speed,
+              humidity: res.data.list[0].main.humidity,
+              sunrise: moment.unix(res.data.city.sunrise).format("HH:MM"),
+              sunset: moment.unix(res.data.city.sunset).format("HH:MM"),
+            }
+            currenComponent.setState({ data: weatherData });
+          })
       }
-    });
+    })
+
   }
 
   render() {
@@ -69,42 +80,3 @@ export default class Card extends React.Component {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-/*   // Props: day, key(index)
-
-  render() {
-    let newDate = new Date();
-    const weekday = this.props.day.dt * 1000
-    newDate.setTime(weekday)
-
-    const imgURL = "owf owf-2x owf-"+ this.props.day.weather[0].id +" owf-5x red"
-
-    // const farenheit = (parseInt(this.props.day.main.temp) - 273.15) * (9/5) + 32
-
-
-    return (
-      <div className="col-md-2 d-inline-flex align-content-end jumbotron">
-        <div className="card bg-light text-center">
-          <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
-          <p className="text-muted">{moment(newDate).format('MMMM Do, H:MM')}</p>
-          <i className = {imgURL}></i>
-          <h2>{Math.round(this.props.day.main.temp)} °C</h2>
-          <div className="card-body">
-            <p className="card-text">{this.props.day.weather[0].description}</p>
-            <p className="card-text">Feels like: {this.props.day.main.feels_like}</p>
-            <p className="card-text">Wind force: {this.props.day.wind.speed}</p>
-            <p className="card-text">Humidity: {this.props.day.main.humidity}</p>
-          </div>
-        </div>
-      </div>
-    )
-  }
-} */
