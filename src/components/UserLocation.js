ec7ed20cf74bb32f8eb4aaa16f080f23c1ee8a21
jpablo1980 <esperix@gmail.com>
2020-04-10 14:29:12 +0200

    Add weather icon next to temperature on the main card

--- src/components/UserLocation.js
+++ src/components/UserLocation.js
@@ -1,20 +1,35 @@
-import React from 'react';
-
+import React, {reading} from 'react';
+import 'weather-icons/css/weather-icons.css';
 
 export default function UserLocation(props) {
 
+    
+    const { temperature, description, location, country, wind_speed, sunrise, sunset, humidity, icon } = props.weatherData;
+    
 
-    const { temperature, description, location, country, wind_speed, sunrise, sunset, humidity, } = props.weatherData;
+
+    const weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    };
+
+    
 
 
     return (
         <div>
             <div className="container mt-3 bg-light text-center">
-                <div className="mt-4">
-                    <h1>{temperature}<sup>o</sup>C<br></br>{description}</h1>
-                    <h4>{location}, {country}</h4>
+                <div className="container d-flex col-3 text-center mt-4">
+                    <h1 className="display-4">{temperature}<sup>o</sup>C <br>
+                    </br>{description}</h1> 
+                        <i className= {`wi ${weatherIcon[icon]} container display-1`}></i>
                 </div>
-            
+                    <h4 className="display-5">{location}, {country}</h4>
             <br></br>
 
             <div className="row">
