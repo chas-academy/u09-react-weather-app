1db8cb979f035c1824ed69d92d40fcc1f032b89f
jpablo1980 <esperix@gmail.com>
2020-04-12 00:12:33 +0200

    Add if condition to today forecast

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import Forecast from './components/Forecast';
+import { MainCard } from './components/MainCard';
 
 
 export default class App extends React.Component {
---
+++ src/components/MainCard.tsx
@@ -0,0 +1,24 @@
+import React, {useState, useEffect } from 'react';
+import { usePosition } from 'use-position';
+import Axios from 'axios';
+
+var moment = require('moment');
+
+export const MainCard = () =>  {
+    const [ state, setState ] = useState({data: null, loading: true});
+    const [ weather, setWeather ] = useState({});
+    const { latitude, longitude } = usePosition(true);
+    
+
+    useEffect(() => {
+        
+    });
+
+
+  return (
+    <code>
+      latitude: {latitude}<br/>
+      longitude: {longitude}<br/>
+    </code>
+  );
+}
