cb5d058d7839e66151562e004a374afcd6f9e202
Emil Babra <brittsboemil@gmail.com>
2020-04-13 20:46:41 +0200

    Buttons in header now console.logs value of pressed button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = props => {
+class Header extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            units: 'metric',
+            metric: "metric",
+            imperial: "imperial"
+        }
+    }
 
+
+    unitChanger(e) {
+        console.log(e.target.value);
+        this.setState({
+            units: e.target.value
+        })
+    }
+
+    
  
-    return (
-        <article>
-            <header className = "text-center">
-                <h1>Weather App</h1>
-                <div className="btn-container">
-                    <button>Celcius</button>
-                    <button>Fahrenheit</button>
-                </div>
-            </header>
-        </article>
-    );
+    render() {
+        return (
+            <article>
+                <header className = "text-center">
+                    <h1>Weather App</h1>
+                    <div className="btn-container">
+                        <button onClick={(e) => this.unitChanger(e)} value="metric">Celcius</button>
+                        <button onClick={(e) => this.unitChanger(e)} value="imperial">Fahrenheit</button>
+                    </div>
+                </header>
+            </article>
+        );
+    }
+    
 }
 
 export default Header;
