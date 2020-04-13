156a7c2e2d95242641296680e297b1049090f678
jpablo1980 <esperix@gmail.com>
2020-04-13 17:34:09 +0200

    Install moment npm install --save moment react-moment

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ export default class App extends React.Component {
   render(){
     return(
       <main >
-        <Header />
-       
-        <Card />
-        <Forecast />
+     
+       <MainCard />
+     
+      
         <section className="d-flex flex-row justify-content-center">
         </section>
       </main>
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,36 +1,45 @@
 import React, {useState, useEffect } from 'react';
 import { usePosition } from 'use-position';
-import Axios from 'axios';
-import {useAsyncEffect} from 'use-async-effect';
+import axios from 'axios';
 var moment = require('moment');
 
-
-const useFetch = (url: RequestInfo) => {
-  const [ data, setData ] = useState({});
-  const [ loading, setLoading ] = useState(true);
-
+export const MainCard =  () =>  {
   
-  useAsyncEffect( async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const [items] = data.results;
-    console.log(data);
-    setData(items);
-    setLoading(false);
-
-  }, []);
-
-  return {data, loading};
+  const [ weather, setWeather ] = useState({
+    location: null,
+    country: null,
+    temperature: null,
+    description: null,
+    wind_speed: null,
+    humidity: null,
+    sunrise: null,
+    sunset: null,
+    icon: null,
+  });
+  const { latitude, longitude } = usePosition(true);
   
-}
-
-
-export const MainCard = () =>  {
-  
-
-  const {data, loading } = useFetch(`https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=135&units=metric&appid=5a274b56354a707ffc91ac0c8eec0c72`);
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=135&units=metric&appid=509cfcbddeec47511025951278d57f93`;
+    axios.get(url)
+    .then(res => setWeather({
+      location: res.data.city.name,
+      country: res.data.city.country,
+      temperature: res.data.list[0].main.temp,
+      description: res.data.list[0].weather[0].main,
+      wind_speed: res.data.list[0].wind.speed,
+      humidity: res.data.list[0].main.humidity,
+      sunrise: moment.unix(res.data.city.sunrise).format("HH:MM"),
+      sunset: moment.unix(res.data.city.sunset).format("HH:MM"),
+      icon: res.data.list[0].weather[0].main,
+    }))
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+ 
 
   return (
-    <div>{loading}</div>
+    <div>hej</div>
+    
   );
 }
