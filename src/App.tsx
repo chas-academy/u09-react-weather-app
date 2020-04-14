178c7fc8fc67e38f9fc08a9a90f83b8cd076f1d6
jpablo1980 <esperix@gmail.com>
2020-04-14 09:44:03 +0200

    Erase file Card.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Card from './components/Card';
 import Forecast from './components/Forecast';
 import { MainCard } from './components/MainCard';
 
